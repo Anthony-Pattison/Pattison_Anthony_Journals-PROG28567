251e7a2ae31baea132896c86300faacbe695d4ab
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-09-02 23:27:32 -0400

    Task one done and saving the scene

--- Programming for Game Design/Assets/Scripts/DrawingSquares.cs
+++ Programming for Game Design/Assets/Scripts/DrawingSquares.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class DrawingSquares : MonoBehaviour
 {
+    public GameObject Prefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Prefab = this.gameObject;
     }
 
     // Update is called once per frame
@@ -16,5 +17,20 @@ public class DrawingSquares : MonoBehaviour
         Vector2 tempscale = transform.localScale;
         tempscale.x = tempscale.x + Input.mouseScrollDelta.y; tempscale.y = tempscale.y + Input.mouseScrollDelta.y;
         transform.localScale = tempscale;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            createCircle(mousePos, tempscale);
+        }
+    }
+
+    private void createCircle(Vector2 temppos, Vector2 tempscale)
+    {
+        
+        GameObject tempsquare =  Instantiate(Prefab);
+        tempsquare.transform.position = temppos;
+        tempsquare.GetComponent<SpriteRenderer>().color = Color.white;
+        tempsquare.GetComponent<DrawingSquares>().enabled = false;
+        tempsquare.transform.localScale = tempscale;
     }
 }
