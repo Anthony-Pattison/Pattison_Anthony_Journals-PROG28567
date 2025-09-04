310f794a375c2bf566d2a088f58c6e63ac7c5250
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-09-04 00:09:42 -0400

    spawns squares form a button press, the amount is based on the amount the user puts in

--- Programming for Game Design/Assets/Scripts/RowGeneration.cs
+++ Programming for Game Design/Assets/Scripts/RowGeneration.cs
@@ -1,7 +1,12 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RowGeneration : MonoBehaviour
 {
+    public GameObject inputfield;
+    public GameObject prefab;
+    float amountofsquares;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,16 @@ public class RowGeneration : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        amountofsquares = float.Parse(inputfield.GetComponent<TMP_InputField>().text);
         
+    }
+
+    public void spawnsquares()
+    {
+        for (int i = 0; i < amountofsquares; i++)
+        {
+            GameObject tempprefab = Instantiate(prefab);
+            tempprefab.transform.position = new Vector2(-9 + i, 0);
+        }
     }
 }
