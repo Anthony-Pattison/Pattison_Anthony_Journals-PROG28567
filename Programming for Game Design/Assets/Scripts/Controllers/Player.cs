0deae6a18f54659b2c174121f07a232d30b184b2
pattisoa <pattisoa@OA-S318-10>
2025-09-11 16:33:38 -0400

    Task one and task two are completed. The trail of bombs and the four corner bombs

--- Programming for Game Design/Assets/Scripts/Controllers/Player.cs
+++ Programming for Game Design/Assets/Scripts/Controllers/Player.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Player : MonoBehaviour
 {
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
+    public int AmountOfBombs;
+    public float bombTrailSpacing;
     [SerializeField] float bombBuffer;
     // Update is called once per frame
     void Update()
@@ -20,14 +23,50 @@ public class Player : MonoBehaviour
         {
             StartCoroutine(SpawnBombAtOffset(playerpos, new Vector3(0, 1, 0), bombBuffer, bombPrefab));
         }
+        if (Input.GetKeyDown("t"))
+        {
+            StartCoroutine(SpawnBombTrailAtOffset(playerpos, new Vector3(0, 1, 0), bombTrailSpacing, bombBuffer, bombPrefab));
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            StartCoroutine(SpawnBombAtOffset(playerpos + RandomNumConerVector(), new Vector3(0, 1, 0), bombBuffer, bombPrefab));
+        }
     }
- 
-    //Vector3 playerpos, Vector3 inOffset, float waitTime, GameObject bombPrefab
+
     IEnumerator SpawnBombAtOffset(Vector3 playerpos, Vector3 inOffset, float waitTime, GameObject bombPrefab)
     {
         yield return new WaitForSeconds(waitTime);
         Vector3 pos = playerpos + inOffset;
         Instantiate(bombPrefab, pos, Quaternion.identity);
+
     }
 
+    IEnumerator SpawnBombTrailAtOffset(Vector3 playerpos, Vector3 inOffset, float spacing, float waitTime, GameObject bombPrefab)
+    {
+        yield return new WaitForSeconds(waitTime);
+        for (int i = 0; i < AmountOfBombs; i++)
+        {
+            Vector3 pos = playerpos - inOffset;
+            pos.y -= spacing * i;
+            Instantiate(bombPrefab, pos, Quaternion.identity);
+        }
+    }
+
+    static Vector3 RandomNumConerVector()
+    {
+        int num = Random.RandomRange(1, 5);
+        switch (num)
+        {
+            case 1:
+                return new Vector3(1, 0.5f);
+            case 2:
+                return new Vector3(1, -2);
+            case 3:
+                return new Vector3(-1, 0.5f);
+            case 4:
+                return new Vector3(-1, -2);
+            default:
+                return new Vector3(0, 0);
+        }
+    }
 }
