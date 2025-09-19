80d93fe59efd437bbdd262146425061320d8cca9
Anthony-Pattison <pattisoa@sheridancollege.ca>
2025-09-18 22:23:53 -0400

    Player movement done and started the asteroid movement

--- Programming for Game Design/Assets/Scripts/Controllers/Asteroid.cs
+++ Programming for Game Design/Assets/Scripts/Controllers/Asteroid.cs
@@ -7,7 +7,7 @@ public class Asteroid : MonoBehaviour
     public float moveSpeed;
     public float arrivalDistance;
     public float maxFloatDistance;
-
+    bool running;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +16,27 @@ public class Asteroid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.position = Vector3.Lerp(transform.position, transform.position + AsteroidMovement(), moveSpeed);
+
+    }
+
+    Vector3 AsteroidMovement()
+    {
+        int randomNum = Random.Range(1, 5);
+        switch (randomNum)
+        {
+            case 1:
+                return new Vector3(1, 1, 0);
+            case 2:
+                return new Vector3(-1, 1, 0);
+            case 3:
+                return new Vector3(1, -1, 0);
+            case 4:
+                return new Vector3(-1, -1, 0);
+            default:
+                break;
+        }
+        return new Vector3(0, 0, 0);
+
     }
 }
--- Programming for Game Design/Assets/Scripts/Controllers/Player.cs
+++ Programming for Game Design/Assets/Scripts/Controllers/Player.cs
@@ -12,26 +12,32 @@ public class Player : MonoBehaviour
     [SerializeField] float speed = 0.01f;
     public Vector3 PlayerSpeed;
     public Vector3 Velocity;
-    float perviousVer = 0;
-    float perviousHorizontal = 0;
     void Update()
     {
         
-        if (Input.GetAxisRaw("Vertical") != 0)
-        {
-            perviousVer = Input.GetAxisRaw("Vertical");
-        }
-        if (Input.GetAxisRaw("Horizontal") != 0)
-        {
-             perviousHorizontal = Input.GetAxisRaw("Horizontal");
-        }
         float acceleration = maxSpeed / accelerationTime;
         Velocity += Input.GetAxisRaw("Horizontal") * acceleration * Time.deltaTime * Vector3.right;
         Velocity += Input.GetAxisRaw("Vertical") * acceleration * Time.deltaTime * Vector3.up;
-        Debug.Log(perviousVer);
 
+        if (Input.GetAxisRaw("Horizontal") == 0 && Velocity.x >= 0.01f)
+        {
+            Velocity +=  acceleration * Time.deltaTime * Vector3.left;
+        }
+        if (Input.GetAxisRaw("Horizontal") == 0 && Velocity.x <= -0.01f)
+        {
+            Velocity += acceleration * Time.deltaTime * Vector3.right;
+        }
+        if (Input.GetAxisRaw("Vertical") == 0 && Velocity.y >= 0.01f)
+        {
+            Velocity += acceleration * Time.deltaTime * Vector3.down;
+        }
+        if (Input.GetAxisRaw("Vertical") == 0 && Velocity.y <= -0.01f)
+        {
+            Velocity += acceleration * Time.deltaTime * Vector3.up;
+        }
         Velocity = Vector3.ClampMagnitude(Velocity, maxSpeed);
         transform.position += Velocity * Time.deltaTime;
+        playerOUtdiseofscreen(transform.position);
     }
 
     private void playerOUtdiseofscreen(Vector2 playerpos)
